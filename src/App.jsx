5d5ab944bbb2b2c53b958fa2f672901b69833cb9
Willian Henrique Arantes <willhenrique791@gmail.com>
2025-01-17 19:56:31 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import "./style/App.css";
+
+const quotes = [
+  { texto: "A persistência é o caminho do êxito.", autor: "Charles Chaplin" },
+  {
+    texto: "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+    autor: "Robert Collier",
+  },
+  { texto: "Só sei que nada sei.", autor: "Sócrates" },
+  {
+    texto: "Seja a mudança que você deseja ver no mundo.",
+    autor: "Mahatma Gandhi",
+  },
+  {
+    texto: "A vida é 10% o que acontece comigo e 90% como eu reajo a isso.",
+    autor: "Charles Swindoll",
+  },
+  {
+    texto: "Nada é impossível, a palavra em si diz: 'Eu sou possível!'.",
+    autor: "Audrey Hepburn",
+  },
+  { texto: "Se você pode sonhar, você pode fazer.", autor: "Walt Disney" },
+  { texto: "A mente é tudo. Você se torna aquilo que pensa.", autor: "Buda" },
+  {
+    texto: "É melhor conquistar a si mesmo do que vencer mil batalhas.",
+    autor: "Buda",
+  },
+  {
+    texto: "A única forma de fazer um ótimo trabalho é amar o que você faz.",
+    autor: "Steve Jobs",
+  },
+  {
+    texto:
+      "O aprendizado é um tesouro que seguirá seu dono em todos os lugares.",
+    autor: "Provérbio chinês",
+  },
+  {
+    texto:
+      "Grandes realizações não são feitas por impulso, mas por uma soma de pequenos esforços.",
+    autor: "Vincent Van Gogh",
+  },
+];
+
+function App() {
+  // cria um estado currentQuote e uma função setCurrentQuote, para atualizar o estado
+  const [currentQuote, setCurrentQuote] = useState(quotes[0]);
+
+  //quando clicar no button chamara essa função e atualizara o estado atual e
+  // vai renderizar novamente
+  const getQuote = () => {
+    const indexRandom = Math.floor(Math.random() * quotes.length);
+    setCurrentQuote(quotes[indexRandom]);
+  };
+  return (
+    <>
+      <div className="container">
+        <h1>{currentQuote.texto}</h1>
+        <p>- {currentQuote.autor}</p>
+        <div>
+          <button onClick={getQuote}>Nova citação</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
